1ff79b903e7a1ea0535dab6745904b502bdda02e
Bernd Fix <brf@hoi-polloi.org>
2022-09-01 17:48:09 +0200

    network: return more tables & statistics.

--- sim/network.go
+++ sim/network.go
@@ -26,10 +26,12 @@ import (
 )
 
 type Network struct {
-	nodes  map[string]*SimNode
-	queue  chan core.Message
-	reach2 float64
-	active bool
+	nodes   map[string]*SimNode
+	queue   chan core.Message
+	reach2  float64
+	trafOut uint64
+	trafIn  uint64
+	active  bool
 }
 
 func NewNetwork(numNodes int, width, length, r2 float64) *Network {
@@ -53,10 +55,13 @@ func (n *Network) Run() {
 	n.active = true
 	for n.active {
 		msg := <-n.queue
+		mSize := uint64(msg.Size())
+		n.trafOut += mSize
 		if sender, ok := n.nodes[msg.Sender().Key()]; ok {
 			for _, node := range n.nodes {
 				dist2 := node.pos.Distance2(sender.pos)
 				if dist2 < n.reach2 && !node.PeerID().Equal(sender.PeerID()) {
+					n.trafIn += mSize
 					go node.Receive(msg)
 				}
 			}
@@ -70,9 +75,51 @@ func (n *Network) Stop() {
 
 func (n *Network) Coverage() float64 {
 	total := 0
-	num := len(n.nodes) - 1
+	num := len(n.nodes)
 	for _, node := range n.nodes {
 		total += node.Forwards()
 	}
-	return float64(100*total) / float64(num*num)
+	return float64(100*total) / float64(num*(num-1))
+}
+
+func (n *Network) Traffic() (in, out uint64) {
+	return n.trafIn, n.trafOut
+}
+
+func (n *Network) FullTable() ([][]int, float64) {
+	allHops := 0
+	numRoute := 0
+	num := len(n.nodes)
+	res := make([][]int, num)
+	for i := range res {
+		res[i] = make([]int, num)
+	}
+	look := make(map[string]int)
+	pos := 0
+	for k := range n.nodes {
+		look[k] = pos
+		pos++
+	}
+	for k1, node1 := range n.nodes {
+		i1 := look[k1]
+		for k2, node2 := range n.nodes {
+			if k1 == k2 {
+				res[i1][i1] = -2
+				continue
+			}
+			i2 := look[k2]
+			if next, hops := node1.Forward(node2.PeerID()); hops > 0 {
+				allHops += hops
+				numRoute++
+				ref := i2
+				if next != nil {
+					ref = look[next.Key()]
+				}
+				res[i1][i2] = ref
+			} else {
+				res[i1][i2] = -1
+			}
+		}
+	}
+	return res, float64(allHops) / float64(numRoute)
 }
