7a5f5ea7e356243a0b3ba69c4acc863bde9d2742
Bernd Fix <brf@hoi-polloi.org>
2022-09-13 12:02:11 +0200

    time: use value instead of pointer recipient.

--- core/time.go
+++ core/time.go
@@ -39,33 +39,33 @@ type Time struct {
 }
 
 // Age of the timestamp
-func (t *Time) Age() *Age {
-	return &Age{time.Now().UnixMicro() - t.Val}
+func (t Time) Age() Age {
+	return Age{time.Now().UnixMicro() - t.Val}
 }
 
 // Expired returns true if 't+ttl' is in the past
-func (t *Time) Expired(ttl time.Duration) bool {
+func (t Time) Expired(ttl time.Duration) bool {
 	return (time.Now().UnixMicro() - t.Val) > ttl.Microseconds()
 }
 
 // Before returns true if t is before t2
-func (t *Time) Before(t2 *Time) bool {
+func (t Time) Before(t2 Time) bool {
 	return t.Val < t2.Val
 }
 
 // String returns a human-readabe timestamps
-func (t *Time) String() string {
+func (t Time) String() string {
 	return time.UnixMicro(t.Val).Format(time.RFC1123)
 }
 
 // TimeNow returns the current time
-func TimeNow() *Time {
-	return &Time{Val: time.Now().UnixMicro()}
+func TimeNow() Time {
+	return Time{Val: time.Now().UnixMicro()}
 }
 
 // TimeFromAge returns a time for a given age.
-func TimeFromAge(a *Age) *Time {
-	return &Time{time.Now().UnixMicro() - a.Val}
+func TimeFromAge(a Age) Time {
+	return Time{time.Now().UnixMicro() - a.Val}
 }
 
 //----------------------------------------------------------------------
@@ -76,11 +76,16 @@ type Age struct {
 }
 
 // String returns a human-readabe timestamps
-func (a *Age) String() string {
+func (a Age) String() string {
 	return time.Duration(1000 * a.Val).String()
 }
 
+// Seconds returns the age in seconds.
+func (a Age) Seconds() float64 {
+	return time.Duration(1000 * a.Val).Seconds()
+}
+
 // Size of an age instance (binary representation)
-func (a *Age) Size() uint {
+func (a Age) Size() uint {
 	return 8
 }
