bc1a827cf6eb2d2f601f0d6684b4cedafa033662
Bernd Fix <brf@hoi-polloi.org>
2022-09-27 23:15:43 +0200

    run: compute number of available CPUs.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GOMEMLIMIT=4096MiB GOMAXPROCS=4 ./liti -c tests/config-$1.json -p rt/leatea.pprof 2>&1 | tee rt/log
+GOMAXPROCS=$(($(nproc)-2)) ./liti -c tests/config-$1.json 2>&1 | tee rt/log
